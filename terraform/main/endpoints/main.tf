770b07084f50fa7ef70f2df6736cf6793ecf9441
CoreySsj <104245267+CoreyTayV@users.noreply.github.com>
2025-10-02 01:02:08 -0500

    tags a endpoints

--- terraform/main/endpoints/main.tf
+++ terraform/main/endpoints/main.tf
@@ -1,23 +1,35 @@
 resource "aws_vpc_endpoint" "ssm" {
-    vpc_id       = var.vpc_id
-    service_name = "com.amazonaws.${var.aws_region}.ssm"
-    vpc_endpoint_type = "Interface"
-    subnet_ids   = var.subnet_id
+    vpc_id             = var.vpc_id
+    service_name       = "com.amazonaws.${var.aws_region}.ssm"
+    vpc_endpoint_type  = "Interface"
+    subnet_ids         = var.subnet_id
     security_group_ids = [var.sg_id]
+
+    tags = {
+        Name = "marco-ssm"
+    }
 }
 
 resource "aws_vpc_endpoint" "ssmmessages" {
-    vpc_id       = var.vpc_id
-    service_name = "com.amazonaws.${var.aws_region}.ssmmessages"
-    vpc_endpoint_type = "Interface"
-    subnet_ids   = var.subnet_id
+    vpc_id             = var.vpc_id
+    service_name       = "com.amazonaws.${var.aws_region}.ssmmessages"
+    vpc_endpoint_type  = "Interface"
+    subnet_ids         = var.subnet_id
     security_group_ids = [var.sg_id]
+
+    tags = {
+        Name = "marco-ssmmessages"
+    }
 }
 
 resource "aws_vpc_endpoint" "ec2messages" {
-    vpc_id       = var.vpc_id
-    service_name = "com.amazonaws.${var.aws_region}.ec2messages"
-    vpc_endpoint_type = "Interface"
-    subnet_ids   = var.subnet_id
+    vpc_id             = var.vpc_id
+    service_name       = "com.amazonaws.${var.aws_region}.ec2messages"
+    vpc_endpoint_type  = "Interface"
+    subnet_ids         = var.subnet_id
     security_group_ids = [var.sg_id]
+
+    tags = {
+        Name = "marco-ec2messages"
+    }
 }
